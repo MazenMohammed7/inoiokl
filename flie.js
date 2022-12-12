65d1c84c909505d2c45ff0469deb6a63bc6386a7
MazenMohammed7 <120386602+MazenMohammed7@users.noreply.github.com>
2022-12-12 19:23:47 +0200

    Add files via upload

---
+++ flie.js
@@ -0,0 +1,143 @@
+// const age = 15;
+
+
+
+// if (age >= 19){
+//    console.log("hello word 🚘")
+// } else{ 
+//     let yearlist = 19 - age ;
+//     console.log(`plase wait ${yearlist} year :)`)
+// }
+
+
+// let x = 2990;
+
+// let y ;
+
+// if (x >= 2000){
+//     y=20
+// } else{
+//     y=21
+// }
+// console.log(y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 1; i < 11 ; i++){
+//     console.log(i)
+// }
+
+// let mark = 21;
+// let jon =  20.9;
+
+
+// if (mark > jon){
+//     console.log(`yes mark top 3n jon`)
+// } else {
+//     console.log(`no jon top 3n mark`)
+// }
+
+
+
+
+
+
+// console.log(Math.floor(100.6))
+
+// console.log(Math.random())
+
+// console.log(parseFloat (100.545).toFixed(1));
+
+
+
+
+
+
+
+// Examples
+// console.log(100_000); // 100000
+// console.log(100000); // 100000
+// console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+// console.log(100_00_0); // 100000
+// console.log(10 ** 5); // 100000
+// console.log(1000 * 100 ); // 100000
+// console.log(1000000 / 10); // 100000
+// console.log(1e5); // 100000
+// console.log(1000000 / 20 * 2); // 100000
+// console.log(Math.ceil(99999.1) ); // 100000
+// console.log(Math.min(100000 , 5000000 , 1200000 , 100000)); // 100000
+// console.log(Math.max(100000,15)); // 100000
+// console.log(10000 + 190000  / 2 -5000); // 100000
+
+
+
+
+
+
+
+
+// console.log(Number.MAX_SAFE_INTEGER + 1);
+// console.log(`${Number.MAX_SAFE_INTEGER}`.length);
+
+
+
+
+
+
+// let myVar = "100.56789 Views";
+
+// console.log(parseInt (myVar)); // 100
+// console.log(Number (parseFloat (myVar).toFixed(2))); // 100.57
+
+
+// let mynumes = [1 , 2 , 3 , 4 , 5 , 6];
+
+// let pushnumes = []
+
+// for  (let i = 0 ; i < mynumes.length ; i++){
+//     pushnumes.push (mynumes[i] *2)
+//     // pushnumes.push (mynumes[i] + mynumes[i])
+// }
+
+
+// console.log(pushnumes)
+
+// let z8lol = {name:"z8lol" , eggs:0 , dek: true};
+// let z8lola = {name:"z8lola" , eggs:5 , dek:false}; 
+// let fred = { name: "fred" , eggs:0 , dek:false};
+// let freda = {name:"freda" , eggs:5 , dek: true}; 
+
+// let a3sha = [z8lol,z8lola,fred,freda];
+
+// function getname(fred){
+//     return a3sha 
+// };
+
+// console.log(getname());
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,537 @@
+// let z8lol = {name:"z8lol" , eggs:0 , dek: true};
+// let zlola = {name:"z8lola" , eggs:5 , dek:false}; 
+// let fred = { name: "fred" , eggs:0 , dek:false};
+// let freda = {name:"freda" , eggs:5 , dek: true}; 
+
+
+// let a3sha = [z8lol,zlola,fred,freda];
+
+// a3sha.push({name:"sho2y" , eggs:5 , dek: true})
+
+
+// let fliter = a3sha.filter(function fliet(value){
+//     if (value.dek === false){
+//         return value
+//     }
+// })
+
+
+// console.log(fliter)
+
+
+
+
+// let mazen =   {name: "mazen" , age:15 , tol: 176 , lon:"smar" ,  male:false} ;
+// let waleed =  {name: "waleed" , age:28 , tol: 179 ,  lon:"smar" , male:false};
+// let osama =   {name: "osama" , age:27 , tol: 179 , lon:"smar" , male:false};
+// let eslam =   {name: "eslam" , age:32 , tol: 179 , lon:"smar" , male:false};
+// let a3omda =  {name: "a3omda" , age:57 , tol: 182 , lon:"smar"  , male:false};
+// let ramadn =  {name: "ramada" , age:48 , tol: 170 , lon:"smar" , male:false};
+// let loza =    {name: "loza" , age:45 , tol: 172 , lon:"smar" , male:true};
+// let roro =    {name: "roro" , age:50 , tol: 168 , lon:"smar", male:true};
+// let aya =     {name: "aya" , age:20 , tol: 160 , lon:"smar", male:true};
+// let gahed =   {name: "gehad" , age:19 , tol: 160 , lon:"smar", male:true};
+// let dody =    {name: "dody" , age:24 , tol: 160 , lon:"smar", male:true};
+// let soso =    {name: "soso" , age:55 , tol: 168 , lon:"smar", male:true} ;
+// let sato =    {name: "sato" , age:87 , tol: 140 , lon:"smar", male:true};
+
+
+// let bett =  [mazen ,waleed ,osama , eslam , a3omda , ramadn , loza ,roro , aya , gahed , dody ,soso ,sato] ;
+ 
+// let filt = bett.filter(function get(value){
+//     if (value){
+//     return value
+//     } 
+    
+// })
+
+// console.log(filt);
+
+
+
+
+
+
+
+
+
+
+// let myfrind = ["mazen","osama","waleed" ,["maro","nour"]];
+
+// console.log(`se hello ${myfrind[3][1][2]}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myfrind = ["mazen","osama","waleed" ,["maro","nour"]];
+
+// console.log(myfrind);
+// myfrind[2] = "MAZEN";
+// console.log(myfrind);
+// myfrind [3] = ["Asmaa","7senn","naser","kosm maro"];
+// console.log(myfrind);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let dataTypes = 10+40;
+// dataTypes += +10;
+// dataTypes ++;
+// dataTypes *= 2
+// console.log(dataTypes)
+
+
+
+
+
+// console.log(100..toString());
+// console.log((100).toString());
+// console.log(parseInt (100.555));
+// console.log(parseFloat (100.545).toFixed(1));
+// console.log(1000.55555.toFixed(3));
+// console.log(isNaN("1000"));
+// console.log(Number.isInteger(100.2))
+
+
+
+
+// console.log()
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(100..toString());
+// console.log((100).toString());
+
+
+// console.log((100.56).toFixed(2));
+
+// console.log(parseInt ("100.56 views"));
+// console.log(parseFloat ("100.56 views"));
+
+
+// console.log(Number.isInteger("100"));
+
+
+// console.log (Math.floor(100.5))
+// console.log (Math.ceil(100.5))
+
+// console.log (Math.round(15.2))
+// console.log (Math.trunc(15.2))
+
+
+// console.log (Math.max(15.2, 100 , 15 ,10000))
+// console.log (Math.min(12.255, 100 , 15 ,10000).toFixed(2))
+
+
+// console.log (Math.pow(10, 2))
+
+
+// console.log (Math.random())
+
+
+
+
+
+// let x = "  mazen  ";
+
+// console.log(x.length)
+
+
+// console.log(x.trim().toUpperCase().charAt(0))
+
+
+// let x = "Elzero Webd School";
+
+// console.log (x.substring(x.length - 5 , x.length - 3))
+
+
+
+
+
+
+
+/*
+  Number Challenge
+*/
+
+// let a = 100;
+// let b = 2_00.5;
+// let c = 1e2;
+// let d = 2.4;
+
+// // Find Smallest Number In All Variables And Return Integer
+// console.log(Math.trunc(Math.min(a , b , c , d)));
+
+// // Use Variables a + d One Time To Get The Needed Output
+// console.log(Math.pow(a , Math.round(d))); // 10000
+
+// // Get Integer "2" From d Variable With 4 Methods
+// console.log(Math.trunc(Math.min(d)));
+// console.log(Math.trunc(Math.max(d)));
+// console.log(Math.trunc(Math.floor(d)));
+// console.log(Math.floor(d));
+
+// // Use Variables b + d To Get This Valus
+// console.log((Math.floor(b) / Math.ceil(d)).toFixed(2)); // 66.67 => String
+// console.log(Math.floor(d + b -135)); // 67 => Number
+
+
+
+
+// console.log(Math.floor  (15.5));
+// console.log(Math.ceil  (15.5));
+// console.log(Math.round  (15.4));
+// console.log(Math.random  ());
+// console.log(Math.trunc (100.5));
+// console.log(Math.pow  (100,2));
+// console.log(Math.max  (100,2.25,100.2));
+// console.log(Math.min  (100,2.25,100.2));
+
+// console.log(Math.pow  (100,2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(parseInt ("150.5 snow"))
+// console.log(parseFloat ("150.5 snow"))
+// console.log(Number.isInteger (15))
+// console.log(Number.isNaN ("mazen" * 1))
+// console.log((110).toString())
+// console.log(100.555 .toFixed(2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(!true)
+// let price = 100;
+// let disc =  true;
+// let discamout =  30;
+// let taleb = false;
+// let contruy = "eg";
+
+// disc === true ? console.log(price -= discamout)  : console.log(price -= discamout + 5) ;
+
+
+// if  (disc === true){
+//   console.log(price -= discamout)  
+
+// } else if (contruy === "ksa"){
+//    console.log(price -= discamout + 5) 
+
+//    if(taleb === true){
+//     console.log(price -= discamout + 30) 
+//    }
+// } else {
+//   console.log("end")
+// }
+
+
+
+
+
+
+
+
+
+
+// let theage =  30;
+
+
+
+
+// theage <= 20 
+// ? console.log (Number ("20"))
+// : theage > 20  && theage < 60 
+// ? console.log ("20 to 60")
+// : theage == 60 
+// ? console.log("max") 
+// : console.log("end") ;
+
+
+
+
+
+// let price = null ;
+
+// console.log(`this is a ${price || "free"}`)
+
+
+// console.log(100 == "100"); // true
+// console.log(100 !== 1000); // true
+// console.log(110 > 100 != 10 != 20); // true
+// console.log(110 > 100 > 10 < 20); // true
+// console.log(-10 == "-10"); // true
+// console.log(typeof -50 === typeof +"-40"); // true
+// console.log(typeof 10 == typeof -"-40"); // true
+// console.log(typeof "10" != typeof 10); // true
+// console.log(typeof 20  !== typeof false); // true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  If Condition Challenge
+*/
+
+// let a = 10;
+
+// if (a < 10) {
+//   console.log(10);
+// } else if (a >= 10 && a <= 40) {
+//   console.log("10 To 40");
+// } else if (a > 40) {
+//   console.log("> 40");
+// } else {
+//   console.log("Unknown");
+// }
+
+// let a = "";
+// a < 20
+//     ?console.log(10)
+//     :a >= 10 && a <= 40
+//     ?console.log("10 To 40")
+//     :a > 40
+//     ?console.log("> 40")
+//     :console.log("Unknown");
+
+
+// Write Previous Condition With Ternary If Syntax
+
+// let st = "Elzero Web School";
+// // console.log(st.slice(0 , 6).repeat(2) )
+
+// // W Position May Change
+// if (typeof st.slice(st.indexOf("W")).toLowerCase() === typeof"w") {
+//   console.log("Good");
+// }
+
+// if (Number(st) !== "string") {
+//   console.log("Good");
+// }
+
+// if (typeof Number(st) === "number") {
+//   console.log("Good");
+// }
+
+// if (st.slice(0 , 6).repeat(2) === "ElzeroElzero") {
+//   console.log("Good");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let theName = ["mazen" , "snow" , "ahmed" , "mazen2" , ["mazen3" , "tassnem"] , "mazen Snow mohmed"];
+
+// // console.log(theName[5][16].toUpperCase())
+
+// theName[5] = "Mohmed"
+// theName[5] = ["Mohmed","mazen"]
+// console.log(theName)
+
+
+
+// let arrOne = ["C", "D", "X"];
+// let arrTwo = ["A", "B", "Z"];
+// let finalArr = [];
+
+// // Write One Single Line Of Code
+// finalArr.push(arrOne)
+// console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let newArray = ["Mazen" , "Snow" , "Ganat","Mazen","roro","Mazen"];
+
+
+
+console.log(newArray.slice(-4 , -1));
+
